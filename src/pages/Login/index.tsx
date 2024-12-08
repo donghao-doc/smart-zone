8579a8b78b699f6a624713d5852d256d119743f9
BarryDong <donghao@bwcj.com>
2024-12-08 22:04:09 +0800

    【登录页】静态页 & 表单

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,70 @@
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import bg from '../../assets/bg.jpg';
+import lgbg from '../../assets/lgbg.jpg';
+import logo from '../../assets/logo.png';
+import './index.scss';
+
+type FieldType = {
+  username?: string;
+  password?: string;
+};
+
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
 function Login() {
   return (
-    <div>
-      登录
+    <div className="login" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="lgbg" style={{ backgroundImage: `url(${lgbg})` }}>
+        <div className="part">
+          <div className="title">
+            <div className="logo">
+              <img src={logo} width={100} alt="logo" />
+            </div>
+            <h1>智慧园区管理平台</h1>
+          </div>
+
+          <Form
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              name="username"
+              rules={[
+                { required: true, message: '用户名不能为空' },
+                { pattern: /^[A-Za-z]{4,8}$/, message: '用户名必须是4-8位英文字母' }
+              ]}
+            >
+              <Input prefix={<UserOutlined />} placeholder="请输入用户名" />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              name="password"
+              rules={[
+                { required: true, message: '密码不能为空' },
+              ]}
+            >
+              <Input.Password prefix={<LockOutlined/>} placeholder="请输入密码" />
+            </Form.Item>
+
+            <Form.Item label={null}>
+              <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
