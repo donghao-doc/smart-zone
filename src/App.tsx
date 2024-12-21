09ec884ceb501920007beed6c837abef38660925
BarryDong <donghao@bwcj.com>
2024-12-21 17:05:11 +0800

    创建基本的路由结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
 function App() {
   return (
     <div className="App">
-      App
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/router/index.tsx
@@ -0,0 +1,23 @@
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const Home = lazy(() => import("../pages/home"));
+const Login = lazy(() => import("../pages/login"));
+const NotFound = lazy(() => import("../pages/404"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
